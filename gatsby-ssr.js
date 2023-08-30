41f92749557ec0baab61630b71afe57fc87f0dec
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-08-30 12:14:59 +0530

    banner

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,14 @@
+import React from 'react';
 exports.onRenderBody = ({ setHtmlAttributes }) => {
   setHtmlAttributes({ lang: "en" })
+}
+
+export function onRenderBody({ setHeadComponents }) {
+setHeadComponents([
+    <script
+      id='cookieyes'
+      type='text/javascript'
+      src='https://cdn-cookieyes.com/client_data/1e4b3b0f9cc2736719714926/script.js'
+    ></script>,
+ ]);
 }
