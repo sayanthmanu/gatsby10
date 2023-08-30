8270eaae7a597b7e92f9ccd05532d8cb317a3a6c
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-08-30 12:23:45 +0530

    3

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,7 @@ setHeadComponents([
     <script
       id='cookieyes'
       type='text/javascript'
-      src='https://cdn-cookieyes.com/client_data/1e4b3b0f9cc2736719714926/script.js'
-    ></script>,
+      src='https://scriptstaging.cookieyes.com/client_data/1e4b3b0f9cc2736719714926/script.js'>
+    </script>,
  ]);
 }
