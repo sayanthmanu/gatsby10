105fa91df8410f1cc5d0b9bf69e0e4ed91be681d
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-09-26 15:14:36 +0530

    embed

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,5 +7,16 @@ setHeadComponents([
       type='text/javascript'
       src='https://scriptstaging.cookieyes.com/client_data/1e4b3b0f9cc2736719714926/script.js'>
     </script>,
+      <div className="video">
+      <iframe
+        src={"https://www.youtube.com/embed/gCuoVMmes4c"}
+        title={"videoTitle"}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowFullScreen
+      />
+    </div>
  ]);
 }
