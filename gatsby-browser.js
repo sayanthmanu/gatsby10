b83db275671751d11bdd6152bdf9f908513e7b82
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-09-28 12:24:46 +0530

    import url redirected to newly created customfont.css in ravenbright.min.css and css-variable.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,2 @@
-// import './src/styles/dist/ravenbright.min.css'
+import './src/styles/dist/ravenbright.min.css'
 
-document.addEventListener("cookieyes_consent_update", function (eventData) {
-  const data = eventData.detail;
-  if (
-    data.accepted.includes("functional") &&
-    !document.getElementById("google-fonts-styles")
-  ) {
-    const head = document.getElementsByTagName("head")[0];
-    const link = document.createElement("link");
-    link.id = "google-fonts-styles";
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href =
-"https://api.fontshare.com/v2/css?f[]=satoshi@700,500,600,400&display=swap";
-    head.appendChild(link);
-  }
-});
