8291b8d80aa9ce79dba7e1bad4b6223ecb88cfdf
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-09-28 11:32:51 +0530

    Google fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -633,7 +633,26 @@ function IndexPage() {
               </div>
             </article>
           </div>
+          <script>
+document.addEventListener("cookieyes_consent_update", function (eventData) {
+  const data = eventData.detail;
+  if (
+    data.accepted.includes("functional") &&
+    !document.getElementById("google-fonts-styles")
+  ) {
+    const head = document.getElementsByTagName("head")[0];
+    const link = document.createElement("link");
+    link.id = "google-fonts-styles";
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    link.href =
+"https://fonts.googleapis.com/css?family=Open+Sans:wght@300&display=swap";
+    head.appendChild(link);
+  }
+});
+</script>
         </div>
+
       </section>
       <CallToAction />
     </Layout>
