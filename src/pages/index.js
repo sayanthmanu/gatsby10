6507c612d9baa790b98a8c533c302a8919ffbb0a
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-09-28 11:36:09 +0530

    Google fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -633,24 +633,7 @@ function IndexPage() {
               </div>
             </article>
           </div>
-          <script>
-document.addEventListener("cookieyes_consent_update", function (eventData) {
-  const data = eventData.detail;
-  if (
-    data.accepted.includes("functional") &&
-    !document.getElementById("google-fonts-styles")
-  ) {
-    const head = document.getElementsByTagName("head")[0];
-    const link = document.createElement("link");
-    link.id = "google-fonts-styles";
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href =
-    "https://api.fontshare.com/v2/css?f[]=satoshi@700,500,600,400&display=swap";
-    head.appendChild(link);
-  }
-});
-</script>
+         
         </div>
 
       </section>
