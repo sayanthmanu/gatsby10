4e31fcc79a13151498d919a919a91aaa0bb5f526
SayanthManu <141239127+SayanthManu@users.noreply.github.com>
2023-11-02 12:15:27 +0530

    transfer to QA2

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function onRenderBody({ setHeadComponents }) {
 setHeadComponents([
-    <script
-      id='cookieyes'
-      type='text/javascript'
-      src='https://scriptstaging.cookieyes.com/client_data/7f9e2171e8279889a470672d/script.js'>
-    </script>,
+    // <script
+    //   id='cookieyes'
+    //   type='text/javascript'
+    //   src='https://scriptstaging.cookieyes.com/client_data/7f9e2171e8279889a470672d/script.js'>
+    // </script>,
       <div className="video">
       <iframe
         src="https://player.vimeo.com/video/861813479?h=4bb45e91be&color=ec4543" 
